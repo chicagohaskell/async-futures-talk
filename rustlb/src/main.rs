50ff21aefb071d2d4c771207f770ef078616fcd6
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-03-14 14:51:34 -0500

    Simplify proxy logic

--- rustlb/src/main.rs
+++ rustlb/src/main.rs
@@ -1,68 +1,63 @@
-
 #![feature(async_await, await_macro, futures_api)]
 
 use std::io;
+use std::net::Shutdown;
 
-use futures::StreamExt;
 use futures::executor::{self, ThreadPool};
 use futures::prelude::*;
-use futures::task::{SpawnExt};
+use futures::task::SpawnExt;
+use futures::StreamExt;
 
 use romio::{TcpListener, TcpStream};
 
 fn main() -> io::Result<()> {
+    executor::block_on(
+        async move {
+            let mut threadpool = ThreadPool::new()?;
 
-    executor::block_on(async move {
-        let mut threadpool = ThreadPool::new()?;
+            let mut listener = TcpListener::bind(&"127.0.0.1:8080".parse().unwrap())?;
+            let mut incoming = listener.incoming();
 
-        let mut listener = TcpListener::bind(&"127.0.0.1:8080".parse().unwrap())?;
-        let mut incoming = listener.incoming();
+            println!("Listening on 127.0.0.1:8080");
 
-        println!("Listening on 127.0.0.1:8080");
+            while let Some(stream) = await!(incoming.next()) {
+                let stream = stream?;
+                let addr = stream.peer_addr()?;
 
-        while let Some(stream) = await!(incoming.next()) {
-            let stream = stream?;
-            let addr = stream.peer_addr()?;
+                let upstreams = vec![
+                    String::from("127.0.0.1:7878"),
+                    String::from("127.0.0.1:7879"),
+                ];
 
-            let upstreams = vec![String::from("127.0.0.1:7878"), String::from("127.0.0.1:7879")];
+                threadpool
+                    .spawn(
+                        async move {
+                            println!("Accepting stream from: {}", addr);
 
-            threadpool.spawn(async move {
-                println!("Accepting stream from: {}", addr);
+                            // Panic on error
+                            await!(load_balance(stream, &upstreams[0])).unwrap();
 
-                // Panic on error
-                await!(load_balance(stream, &upstreams[0])).unwrap();
+                            println!("Closing stream from: {}", addr);
+                        },
+                    )
+                    .unwrap();
+            }
 
-                println!("Closing stream from: {}", addr);
-            }).unwrap();
-        }
-
-        Ok(())
-    })
+            Ok(())
+        },
+    )
 }
 
-async fn load_balance(mut stream: TcpStream, upstream: &str) -> io::Result<()> {
-    let mut upstream = await!(TcpStream::connect(&upstream.parse().unwrap()))?;
-    let mut buf = Vec::with_capacity(100);
+async fn load_balance(stream: TcpStream, upstream: &str) -> io::Result<()> {
+    let upstream = await!(TcpStream::connect(&upstream.parse().unwrap()))?;
+    let (mut reader, mut writer) = stream.split();
+    let (mut ureader, mut uwriter) = upstream.split();
 
-    println!("Making requst");
+    println!("Making request to upstream");
+    await!(reader.copy_into(&mut uwriter)).unwrap();
 
-    // Read into buffer
-    let read = await!(stream.read_exact(&mut buf))?;
+    println!("returning response");
+    await!(ureader.copy_into(&mut writer)).unwrap();
 
-    println!("Got request: {:?}, {:?}", read, buf);
-
-    // Make request
-    await!(upstream.write_all(&buf))?;
-
-    // Clear the buffer
-    buf.clear();
-
-    // Read the response from upstream
-    let read = await!(upstream.read_to_end(&mut buf))?;
-
-    println!("Got response: {:?} {:?}", read, buf);
-
-    // write response back
-    await!(stream.write_all(&buf))?;
     Ok(())
 }
