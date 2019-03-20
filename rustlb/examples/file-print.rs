d66f93c46a76c493ff5558955b5d8a1656a94517
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-03-19 22:50:41 -0500

    Some more experimentation

--- rustlb/examples/file-print.rs
+++ rustlb/examples/file-print.rs
@@ -1,19 +1,24 @@
 use tokio::io::{copy, stdout};
-use tokio::prelude::Future;
+use tokio::prelude::{future::lazy, Future};
 
 fn main() {
-    let task = tokio::fs::File::open("/Users/bigb/vimwiki/index.md")
-        .and_then(|file| {
-            // do something with the file ...
-            copy(file, stdout())
-        })
-        .and_then(|(n, _, _)| {
-            println!("Printed {} bytes", n);
-            Ok(())
-        })
-        .map_err(|e| {
-            // handle errors
-            eprintln!("IO error: {:?}", e);
-        });
-    tokio::run(task);
+    tokio::run(lazy(|| {
+        tokio::spawn(
+            tokio::fs::File::open("/Users/bigb/vimwiki/index.md")
+                .and_then(|file| {
+                    // do something with the file ...
+                    copy(file, stdout())
+                })
+                .and_then(|(n, _, _)| {
+                    println!("Printed {} bytes", n);
+                    Ok(())
+                })
+                .map_err(|e| {
+                    // handle errors
+                    eprintln!("IO error: {:?}", e);
+                }),
+        );
+
+        Ok(())
+    }));
 }
